4779d76c5bdbd1c8ca4af6359522b48a3c327998
Gavin Cone <gcone24@biomedscienceacademy.org>
2020-11-09 18:25:30 +0000

    I made a madlibs program

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Gavin Cone
+Period/Core: 3
 
 
 """
+adjective1 = input("Enter an adjective: ")
+livingCreature = input("Enter a living creature: ")
+verb1 = input("Enter a verb ending in s: ")
+event = input("Enter an event: ")
+verb2 = input("Enter a base form verb: ")
+adjective2 = input("Enter an adjective: ")
+gender = input("Enter he or she: ")
+adverb = input("Enter an adverb: ")
+verb3 = input("Enter a present verb that ends with s: ")
+adjective3 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adjective4 = input("Enter an adjective that ends with ness: ")
+verb4 = input("Enter a verb ending in ed: ")
+type_of_building = input("Enter a type of building: ")
+
+print("A(n) " + adjective1 + " " + livingCreature + " " + verb1 + " in a(n) " + event + " to " + verb2 + " the character from the " + adjective2 + " creature. " + gender + " " + adverb + " " + verb3 + " for the " + adjective3 + " " + noun + " so that " + adjective4 + " can be " + verb4 + " to the " + type_of_building + ".")
